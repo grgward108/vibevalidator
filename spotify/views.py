8733fadf52077a4691b2f235d09e9aefd2931a5f
Edward <edward.effendy@yahoo.com>
2023-06-18 00:58:55 +0900

    more debugging

--- spotify/views.py
+++ spotify/views.py
@@ -77,18 +77,16 @@ def fetch_data(request):
         }
 
         return render(request, "result.html", data)
-    
+
     else:
-        error_message = "Error retrieving data from Spotify.\n"
-        error_message += f"Short term top artists response code: {short_term_top_artists_response.status_code}\n"
-        error_message += f"Long term top artists response code: {long_term_top_artists_response.status_code}\n"
-        error_message += f"User profile response code: {user_profile_response.status_code}\n"
+        short_term_error = short_term_top_artists_response.json()
+        long_term_error = long_term_top_artists_response.json()
+        user_profile_error = user_profile_response.json()
+        error_data = {
+            "short_term_error": short_term_error,
+            "long_term_error": long_term_error,
+            "user_profile_error": user_profile_error,
+        }
+        print(error_data)  # or use logging module
+        return HttpResponse("Error retrieving data from Spotify. Details: " + str(error_data))
 
-        if short_term_top_artists_response.status_code != 200:
-            error_message += f"Short term top artists response: {short_term_top_artists_response.text}\n"
-        if long_term_top_artists_response.status_code != 200:
-            error_message += f"Long term top artists response: {long_term_top_artists_response.text}\n"
-        if user_profile_response.status_code != 200:
-            error_message += f"User profile response: {user_profile_response.text}\n"
-
-        return HttpResponse(error_message)
