39a8fa907bae35029b1e0c7bb41a7af5fc01b7f1
Edward <edward.effendy@yahoo.com>
2023-06-20 10:31:31 +0900

    add more urls

--- spotify/urls.py
+++ spotify/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index2, name="index2"),
     path("spotify-auth/", views.spotify_auth, name="spotify_auth"),
     path("spotify_redirect/", views.spotify_redirect, name="spotify_redirect"),
+    path('refresh_token/', views.refresh_token, name='refresh_token'),
     path("fetch_data/", views.fetch_data, name="fetch_data")
 ]
